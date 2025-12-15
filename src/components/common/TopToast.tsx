d52f861110a75e887db91f901f9803cebe8c96ed
kimddddh98 <amawang11@gmail.com>
2025-12-15 19:07:26 +0900

    feat : 전송 성공여부 토스트 컬러 조정

--- src/components/common/TopToast.tsx
+++ src/components/common/TopToast.tsx
@@ -9,13 +9,13 @@ type TopToastProps = {
 export default function TopToast({ type, message, visible }: TopToastProps) {
   return (
     <div
-      className={`fixed top-6 left-1/2 z-50 -translate-x-1/2 transition-all duration-500 ease-out ${visible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'} pointer-events-none`}
+      className={`fixed top-4 left-1/2 z-50 -translate-x-1/2 transition-all duration-500 ease-out ${visible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'} pointer-events-none`}
     >
       <div
-        className={`min-w-[320px] rounded-2xl border px-6 py-4 text-center text-sm font-medium shadow-xl backdrop-blur-xl ${
+        className={`min-w-[320px] rounded-2xl border px-6 py-4 text-center text-sm font-medium shadow-lg backdrop-blur-xl ${
           type === 'success'
-            ? 'border-emerald-300/30 bg-emerald-400/20 text-emerald-100'
-            : 'border-red-300/30 bg-red-400/20 text-red-100'
+            ? 'border-emerald-400/40 bg-emerald-300/20 dark:bg-emerald-900/20'
+            : 'border-red-400/30 bg-red-400/20 dark:bg-red-900/20'
         } `}
       >
         {message}

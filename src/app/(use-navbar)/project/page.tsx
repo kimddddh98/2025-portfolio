2876a63698ffeffd1255f0c6e95ec0ce7a4f3a80
kimddddh98 <amawang11@gmail.com>
2025-12-17 00:57:57 +0900

    feat : 프로젝트 페이지 레이아웃 수정

--- src/app/(use-navbar)/project/page.tsx
+++ src/app/(use-navbar)/project/page.tsx
@@ -3,17 +3,19 @@ import { PROJECTS } from '@/const/projects.const'
 
 const ProjectPage = () => {
   return (
-    <div className="flex h-full flex-wrap items-center justify-center gap-10">
-      {PROJECTS.map((p, index) => (
-        <ProjectWrap
-          key={p.id}
-          id={p.id}
-          title={p.title}
-          stack={p.stack}
-          ogImage={p.ogImage}
-          url={p.url}
-        />
-      ))}
+    <div className="flex h-full justify-center">
+      <div className="grid h-full w-full max-w-7xl grid-cols-1 content-center items-center gap-16 sm:grid-cols-2 lg:grid-cols-4">
+        {PROJECTS.map((p, index) => (
+          <ProjectWrap
+            key={p.id}
+            id={p.id}
+            title={p.title}
+            stack={p.stack}
+            ogImage={p.ogImage}
+            url={p.url}
+          />
+        ))}
+      </div>
     </div>
   )
 }
